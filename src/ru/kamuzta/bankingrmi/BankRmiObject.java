1411bb5dd937a21e49927c5f70488dcca59988a8
anshjava <ansh.msk@gmail.com>
2020-11-08 16:53:22 +0300

    ver 1.1, added synchronization in BankRmiObject

--- src/ru/kamuzta/bankingrmi/BankRmiObject.java
+++ src/ru/kamuzta/bankingrmi/BankRmiObject.java
@@ -10,83 +10,93 @@ public class BankRmiObject implements BankRmi {
     @Override
     public String createCard(String cardHolderName) throws RemoteException {
         String message = "";
-        Card newCard = new Card(cardHolderName);
-        cardList.add(newCard);
-        message = "Card # " + newCard.getCardNumber() + " created!";
-        System.out.println(message);
-        return message;
+        synchronized (cardList) {
+            Card newCard = new Card(cardHolderName);
+            cardList.add(newCard);
+            message = "Card # " + newCard.getCardNumber() + " created!";
+            System.out.println(message);
+            return message;
+        }
     }
 
     @Override
     public String getCardBalance(long cardNumber) throws RemoteException {
         boolean status = false;
         String message = "";
-        for (Card card : cardList) {
-            if (card.getCardNumber() == cardNumber) {
-                message = card.getCardNumber() + " balance is: " + card.getCardBalance();
-                status = true;
+        synchronized (cardList) {
+            for (Card card : cardList) {
+                if (card.getCardNumber() == cardNumber) {
+                    message = card.getCardNumber() + " balance is: " + card.getCardBalance();
+                    status = true;
+                }
             }
-        }
-        if (!status) {
-            message = "Wrong cardNumber!";
-        }
+            if (!status) {
+                message = "Wrong cardNumber!";
+            }
 
-        System.out.println(message);
-        return message;
+            System.out.println(message);
+            return message;
+        }
     }
 
     @Override
     public String depositMoney(long cardNumber, int money) throws RemoteException {
         boolean status = false;
         String message = "";
-        for (Card card : cardList) {
-            if (card.getCardNumber() == cardNumber) {
-                card.setCardBalance(card.getCardBalance() + money);
-                message = "Added " + money + ". New balance of card " + card.getCardNumber() + " is " + card.getCardBalance();
-                status = true;
+        synchronized (cardList) {
+            for (Card card : cardList) {
+                if (card.getCardNumber() == cardNumber) {
+                    card.setCardBalance(card.getCardBalance() + money);
+                    message = "Added " + money + ". New balance of card " + card.getCardNumber() + " is " + card.getCardBalance();
+                    status = true;
+                }
             }
+            if (!status) {
+                message = "Wrong cardNumber!";
+            }
+            System.out.println(message);
+            return message;
         }
-        if (!status) {
-            message = "Wrong cardNumber!";
-        }
-        System.out.println(message);
-        return message;
     }
 
     @Override
     public String withdrawMoney(long cardNumber, int money) throws RemoteException {
         boolean status = false;
         String message = "";
-        for (Card card : cardList) {
-            if (card.getCardNumber() == cardNumber) {
-                if (money > card.getCardBalance()) {
-                    message = "Not enough money on Card " + card.getCardNumber();
-                    status = true;
-                } else {
-                    card.setCardBalance(card.getCardBalance() - money);
-                    message = money + " withdrawed. New balance of card " + card.getCardNumber() + " is " + card.getCardBalance();
-                    status = true;
+        synchronized (cardList) {
+            for (Card card : cardList) {
+                if (card.getCardNumber() == cardNumber) {
+                    if (money > card.getCardBalance()) {
+                        message = "Not enough money on Card " + card.getCardNumber();
+                        status = true;
+                    } else {
+                        card.setCardBalance(card.getCardBalance() - money);
+                        message = money + " withdrawed. New balance of card " + card.getCardNumber() + " is " + card.getCardBalance();
+                        status = true;
+                    }
                 }
             }
+            if (!status) {
+                message = "Wrong cardNumber!";
+            }
+            System.out.println(message);
+            return message;
         }
-        if (!status) {
-            message = "Wrong cardNumber!";
-        }
-        System.out.println(message);
-        return message;
     }
 
     @Override
     public String getCardList() throws RemoteException {
         StringBuilder sb = new StringBuilder();
         String message;
-        for (Card card : cardList) {
-            sb.append(card.toString());
-            sb.append("\n");
-        }
-        message = sb.toString();
+        synchronized (cardList) {
+            for (Card card : cardList) {
+                sb.append(card.toString());
+                sb.append("\n");
+            }
+            message = sb.toString();
 
-        System.out.println(message);
-        return message;
+            System.out.println(message);
+            return message;
+        }
     }
 }
